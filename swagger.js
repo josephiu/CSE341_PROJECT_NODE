65bdf51522ead98947aa3d8a0ca23584bff7af9a
josephiu <iu.joseph37@gmail.com>
2025-03-22 21:40:08 +0400

    swagger updated url

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         tittle:'Users Api',
         description:'Users Api'
     },
-    host:'localhost:3000',
+    host:'cse341-project-node.onrender.com',
     schemes:['http','https']
 };
 
